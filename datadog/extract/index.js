b2969e1940936bc179fc361610756a62f8bfcb7f
kimjinsub <friday@softworks.co.kr>
2024-08-28 21:41:33 +0900

    datadog extract nodejs - querystring, method, kor_timezone

--- datadog/extract/index.js
+++ datadog/extract/index.js
@@ -1,20 +1,22 @@
 const xlsx = require('xlsx');
-const filename = 'ICAS3163_bom.csv'
+const filename = 'easy.csv'
 const workbook = xlsx.readFile(__dirname + '/xlsx/' + filename)
 const sheetName = workbook.SheetNames[0]
 const result = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName])
 const arr = [];
 result.forEach((row, idx) => {
   let temp = {
-    date: row['Date'],
     host: row['Host'],
     service: row['Service'],
+    date: (new Date(row['Date'])).toLocaleString(),
   };
   const message = row['Message'];
   // if (idx === 0 ) console.log(message);
 
   let reg = {
+    commandMethod: /(?<="method":").*?(?=")/g,
     referer: /(?<=(referer":"))(.*?)(?=")/g,
+    referer2: /.*(?=\?)/g,
     commandPath: /(?<=(command.path : ))(.*?)(?= ,)/g,
     commandUrl: /(?<=(options : {"url":"))(.*?)(?=")/g,
     xNodeUrl: /(?<=("x-node-url":"))(.*?)(?=")/g,
@@ -27,7 +29,20 @@ result.forEach((row, idx) => {
   };
 
   // const res = message.match(regexr).join('')
+  temp.commandMethod = (message.match(reg.commandMethod)||[])?.[0];
   temp.referer = (message.match(reg.referer))?.[0];
+  let str = '{';
+  if (temp.referer) {
+    let url = new URL(temp.referer);
+    let params = url.searchParams;
+    let keys = params.keys();
+    for(let key of keys) {
+      str += `"${key}":"${params.get(key)}",`;
+    }
+  }
+  str = str.length > 1 ? str.substring(0, str.length-1) + '}' : '';
+  temp.referer = ((temp.referer||'').match(reg.referer2)||[])?.[0];
+  temp.query = str;
   temp.commandPath = (message.match(reg.commandPath)||[])?.[0];
   temp.commandUrl = (message.match(reg.commandUrl)||[])?.[0];
   temp.xNodeUrl = (message.match(reg.xNodeUrl)||[])?.[0];
@@ -37,6 +52,7 @@ result.forEach((row, idx) => {
   temp.mbrChlId = (message.match(reg.mbrChlId)||[])?.[0];
   temp.errCode = (message.match(reg.errCode)||[])?.[0];
   temp.errMsg = (message.match(reg.errMsg)||[])?.[0];
+  temp.message = message;
 
   // if (idx === 0 ) console.log(temp);
   arr.push(temp);
